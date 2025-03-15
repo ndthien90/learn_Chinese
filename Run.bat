ad6a57bd04d45d17ab00454a2516bad9be1e09a2
ndthien90 <ndthien@foxmail.com>
2025-03-15 17:16:37 +0700

    111

---
+++ Run.bat
@@ -0,0 +1,9 @@
+@echo off
+:: Khởi động server HTTP của Python trong một cửa sổ mới và chạy ngầm
+start "" python -m http.server
+
+:: Đợi vài giây để server khởi động
+timeout /t 1 > nul
+
+:: Mở địa chỉ localhost:8000 trong trình duyệt mặc định
+start http://localhost:8000/main.html
